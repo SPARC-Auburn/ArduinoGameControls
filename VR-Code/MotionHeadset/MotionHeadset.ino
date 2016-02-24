909ceb23ac32d198b52ccddb73c0a079b402ee27
steffen <sbs0034@tigermail.auburn.edu>
2016-02-24 07:57:53 -0600

    deleted some unneeded code

--- VR-Code/MotionHeadset/MotionHeadset.ino
+++ VR-Code/MotionHeadset/MotionHeadset.ino
@@ -2,10 +2,6 @@
 #include <L3G.h>
 #include <LSM303.h>
 #include <Servo.h>
-int Ping1 = 10;
-int Ping2 = 11;
-int Ping3 = 12;
-long duration1, duration2, duration3 ,inches_left, inches_right, inches_up, cm_left, cm_right, cm_up;
 double centery= 103;
 double centerx= 90;
 double r;
@@ -91,39 +87,6 @@ void FPS()
 	// 	Mouse.move(((((int)gyro.g.y * xkp2) + 1.23)), 0, 0);
 	// }
 	
-	// void Octagon()
-	// {
-	// 	ReadPings();
-
-
-	// 	if(inches_left <= 5)
-	// 	{
-	// 		Keyboard.write('a');
-	// 		while(inches_left <= 5)
-	// 		{
-	// 			ReadPings();
-	// 		}
-
-	// 	}
-
-	// 	if(inches_right <= 5)
-	// 	{
-	// 		Keyboard.write('d');
-	// 		while(inches_right <= 5)
-	// 		{
-	// 			ReadPings();
-	// 		}
-	// 	}
-
-	// 	if(inches_up <= 2)
-	// 	{
-	// 		Keyboard.write('w');
-	// 		while(inches_up <= 2)
-	// 		{
-	// 			ReadPings();
-	// 		}
-	// 	}
-	// }
 
 	// void PinBall()
 	// {
@@ -160,58 +123,7 @@ void FPS()
 	// 	last_difference = difference;
 	// }
 
-	// void ReadPings()
-	// {
-	// 	pinMode(Ping1, OUTPUT);
-	// 	digitalWrite(Ping1, LOW);
-	// 	delayMicroseconds(2);
-	// 	digitalWrite(Ping1, HIGH);
-	// 	delayMicroseconds(5);
-	// 	digitalWrite(Ping1, LOW);
-	// 	pinMode(Ping1, INPUT);
-	// 	duration1 = pulseIn(Ping1, HIGH);
-	// 	inches_left = microsecondsToInches(duration1);
-	// 	cm_left = microsecondsToCentimeters(duration1);
-
-	// 	delay(10);
-
-	// 	pinMode(Ping2, OUTPUT);
-	// 	digitalWrite(Ping2, LOW);
-	// 	delayMicroseconds(2);
-	// 	digitalWrite(Ping2, HIGH);
-	// 	delayMicroseconds(5);
-	// 	digitalWrite(Ping2, LOW);
-	// 	pinMode(Ping2, INPUT);
-	// 	duration2 = pulseIn(Ping2, HIGH);
-	// 	inches_right = microsecondsToInches(duration2);
-	// 	cm_right = microsecondsToCentimeters(duration2);
-
-	// 	delay(10);
-
-	// 	pinMode(Ping3, OUTPUT);
-	// 	digitalWrite(Ping3, LOW);
-	// 	delayMicroseconds(2);
-	// 	digitalWrite(Ping3, HIGH);
-	// 	delayMicroseconds(5);
-	// 	digitalWrite(Ping3, LOW);
-	// 	pinMode(Ping3, INPUT);
-	// 	duration3 = pulseIn(Ping3, HIGH);
-	// 	inches_up = microsecondsToInches(duration3);
-	// 	cm_up = microsecondsToCentimeters(duration3);
-
-	// 	delay(10);
-
-
-	// }
-	// long microsecondsToInches(long microseconds)
-	// {
-	// 	return microseconds / 74 / 2;
-	// }
-
-	// long microsecondsToCentimeters(long microseconds)
-	// {
-	// 	return microseconds / 29 / 2;
-	// }
+	
 	void Debug()
 	{
 		gyro.read();
